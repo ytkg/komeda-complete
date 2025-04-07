08ae6c7b738ca16e8916ba1876cccf2b90c3a29c
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2025-04-07 09:26:20 +0900

    feat: category filter

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,9 @@ function MenuList() {
     }));
   };
 
+  const categories = ['All', 'Food', 'Drink', 'Dessert']
+  const [category, setCategory] = useState(categories[0])
+
   const fetcher = (url: string) => fetch(url).then(res => res.json())
   const { data: items, isLoading } = useSWRImmutable<Item[]>('https://komeda-api-20241013-d054cdaa7331.herokuapp.com/', fetcher)
 
@@ -31,11 +34,14 @@ function MenuList() {
     return <progress className="circle"></progress>
   }
 
-  const filteredItems = items.filter((item) => item.completed_at ? checkedItems.complete : checkedItems.incomplete)
+  const filteredItems = items.filter((item) => {
+    const category_map: { [key: string]: string } = { 'Food': '1-4', 'Drink': '1-2', 'Dessert': '1-3' }
+    return (item.completed_at ? checkedItems.complete : checkedItems.incomplete) && (category === 'All' ? true : category_map[category] === item.large_type)
+  })
 
   return(
     <>
-      <div className="field middle-align">
+      <div className="padding">
         <nav>
           <label className="checkbox">
             <input type="checkbox" checked={checkedItems.incomplete} onChange={() => handleChange('incomplete')} />
@@ -46,6 +52,13 @@ function MenuList() {
             <span>済</span>
           </label>
         </nav>
+      </div>
+      <div>
+        <div className="tabs">
+          {categories.map((c) => {
+            return <a className={category === c ? 'active' : ''} onClick={() => setCategory(c)}>{c}</a>
+          })}
+        </div>
       </div>
       <div className="grid">
         {filteredItems.map((item: Item) => {
